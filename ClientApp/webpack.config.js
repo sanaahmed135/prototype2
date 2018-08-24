8355dcec7f64880896a97b9d06dc4133b2dcbfc3
Sana Saad <s.saad@solvin.com>
2018-08-24 09:56:51 +0200

    Second Commit

--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -1,62 +1,95 @@
 
-const path = require('path'),
+var path = require("path");
 
-webpack = require('webpack'),
+var config = {
 
-HtmlWebpackPlugin = require('html-webpack-plugin');
+    entry: { 'main': './src/app/App.tsx' },
+    resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
 
 
+  output: {
+    path: path.resolve(__dirname, "wwwroot"),
+    filename: "bundle.js"
+  },
 
-module.exports = {
+  module: {
 
-entry: {
-
-    app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
-
-    vendor: ['react', 'react-dom']
-
-},
-
-output: {
-
-    path: path.resolve(__dirname, 'dist'),
-
-    filename: 'js/[name].bundle.js'
-
-},
-
-devtool: 'source-map',
-
-resolve: {
-
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
-
-},
-
-module: {
 
     rules: [
 
-        {
+      {
 
-            test: /\.(ts|tsx)$/,
-
-            loader: 'ts-loader'
-
-        },
-
-        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
     ]
 
-},
+  }
 
-plugins: [
+};
 
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'app', 'index.html') }),
 
-    new webpack.HotModuleReplacementPlugin()
 
-]
+module.exports = config;
+// const path = require('path'),
 
-}
+// webpack = require('webpack'),
+
+// HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+// module.exports = {
+
+// entry: {
+
+//     app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
+
+//     vendor: ['react', 'react-dom']
+
+// },
+
+// output: {
+
+//     path: path.resolve(__dirname, 'dist'),
+
+//     filename: 'js/[name].bundle.js'
+
+// },
+
+// devtool: 'source-map',
+
+// resolve: {
+
+//     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+
+// },
+
+// module: {
+
+//     rules: [
+
+//         {
+
+//             test: /\.(ts|tsx)$/,
+
+//             loader: 'ts-loader'
+
+//         },
+
+//         { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+
+//     ]
+
+// },
+
+// plugins: [
+
+//     new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'app', 'index.html') }),
+
+//     new webpack.HotModuleReplacementPlugin()
+
+// ]
+
+// }
